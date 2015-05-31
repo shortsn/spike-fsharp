c2ac32be59d77f2eb95204d8ea17a39d701d7e8d
shortsn <shortsn@users.noreply.github.com>
2015-05-31 14:01:52 +0200

    jira api

--- jira_rest.fsx
+++ jira_rest.fsx
@@ -1,23 +1,30 @@
 #r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 
+namespace JiraHelper
+
 open System
 open System.Net
 open FSharp.Data
 open FSharp.Data.JsonExtensions
 
-type Jira = {
-    Server : string
-    Api : string
-    User : string
-    Password : string
-}
+[<AutoOpen>]
+module Jira =
+  type Api = {
+      Server : string
+      Api : string
+      User : string
+      Password : string
+  }
 
-let jira_search (jira: Jira) jql max_results =
-  let url = sprintf "%s/%s/search?%s&fields=id,key,summary&maxResults=%i" jira.Server jira.Api jql max_results
-  Http.RequestString(url,
-    httpMethod = HttpMethod.Get,
-    headers = [
-      HttpRequestHeaders.UserAgent ""
-      HttpRequestHeaders.ContentType HttpContentTypes.Json
-      HttpRequestHeaders.BasicAuth jira.User jira.Password])
-  |> JsonValue.Parse
+  let Search (jira: Api) jql max_results =
+    let url = sprintf "%s/%s/search?%s&fields=id,key,summary&maxResults=%i" jira.Server jira.Api jql max_results
+    Http.RequestString(url,
+      httpMethod = HttpMethod.Get,
+      headers = [
+        HttpRequestHeaders.UserAgent ""
+        HttpRequestHeaders.ContentType HttpContentTypes.Json
+        HttpRequestHeaders.BasicAuth jira.User jira.Password])
+    |> JsonValue.Parse
+
+  type Api with
+    member this.Search = Search this
